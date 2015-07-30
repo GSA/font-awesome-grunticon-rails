c1b8690c26e0db9cd1bf5d5e31cba40b11645d38
Nick Marden <nick@marden.org>
2015-07-30 12:53:01 -0400

    Build grunticon-enabled version of Font Awesome fonts, without any need for webfonts

---
+++ lib/font/awesome/grunticon/rails.rb
@@ -0,0 +1,11 @@
+require "font/awesome/grunticon/rails/version"
+
+module Font
+  module Awesome
+    module Grunticon
+      module Rails
+        class Engine < ::Rails::Engine; end
+      end
+    end
+  end
+end
